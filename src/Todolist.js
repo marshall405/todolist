de71e17d2bf226752d612f841282091c6e7ca4be
Marshall Slemp <marshall.slemp@gmail.com>
2018-08-29 15:26:41 -0500

    add styles folder, install material icons

--- src/Todolist.js
+++ src/Todolist.js
@@ -1,41 +1,88 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 // import Material-UI
 import Input from '@material-ui/core/Input';
+import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
-import Checkbox from '@material-ui/core/Checkbox';
+import ListItemText from '@material-ui/core/ListItemText';
+
+import Button from '@material-ui/core/Button';
+import AddIcon from '@material-ui/icons/Add';
+import DoneIcon from '@material-ui/icons/Done';
+import EditIcon from '@material-ui/icons/Edit';
+import DeleteIcon from '@material-ui/icons/Delete';
+
+
 // import own css
 import './styles/todolist.css';
 
 
-export default class List extends React.Component {
+export default class ToDoList extends React.Component {
   state = {
     list : [],
-    currentValue : ''
+    currentValue : '',
+  }
+  addItem(item){
+    const { list } = this.state;
+    const newList = [...list];
+    newList.push(item);
+    this.setState({ 
+      list : newList,
+      currentValue : ''
+    });
   }
   handleOnChange(e){
     return this.setState({currentValue : e.target.value})
   }
   handleSubmit(e){
     e.preventDefault();
-    const newList = this.state.list.slice();
-    newList.push(this.state.currentValue.trim());
-    this.setState({ 
-      list : newList,
-      currentValue : ''
-    });
+    const item = this.state.currentValue.trim();
+    if(item){
+      this.addItem(item);
+    }
     e.target.reset();
+  } 
+  handleDelete(index){
+    const newList = this.state.list.slice();
+    let removedItems;
+    newList.splice(index, 1);
+    this.setState({list : newList});
+  }
+  handleDone(index){
+    console.log(this.state.list[index])
+    
+    
+  }
+  handleEdit(index){
+    const changeItem = prompt(`Edit ${this.state.list[index]}`);
+    const newList = this.state.list.slice();
+    newList[index] = changeItem;
+    this.setState({list : newList});
   }
   renderList() {
     return (
-      <ul>
-        { this.state.list.map((item, index) => {
+      <List>
+        { 
+          this.state.list.map((item, index) => {
             return (
-             <ListItem key={index}> {item} <Checkbox /></ListItem>
+              <ListItem button={true} divider={true} key={index}>
+                <ListItemText> 
+                  {item} 
+                </ListItemText>
+                <Button className='done' variant='contained' onClick={() => this.handleDone(index)}>
+                  <DoneIcon/>
+                </Button>
+                <Button className='edit' variant='contained' onClick={() => this.handleEdit(index)}>
+                  <EditIcon />
+                </Button>
+                <Button className='delete' variant='contained' onClick={() => this.handleDelete(index)}>
+                  <DeleteIcon/>
+                </Button>
+              </ListItem>
             ) 
           })
         }
-      </ul>
+      </List>
     )
   }
   render(){
@@ -43,8 +90,19 @@ export default class List extends React.Component {
       <div className='listContainer'>
         <h1> To Do List </h1>
         <form onSubmit={(e) => this.handleSubmit(e)}>
-          <Input type='text' placeholder='add item' onChange={(e) => this.handleOnChange(e)} />
-          
+          <Input 
+            type='text' 
+            placeholder='add item' 
+            onChange={(e) => this.handleOnChange(e)} 
+          />
+          <Button 
+            className='add' 
+            variant='contained' 
+            size='small' 
+            type='submit'
+          >
+            <AddIcon />
+          </Button>
         </form>
         {this.renderList()}
       </div>
